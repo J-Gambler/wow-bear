0c1a33ec4fadfbf66b6e2db48663329838d5dc3b
J-Gambler <anotovserg943@gmail.com>
2022-03-31 21:35:28 +0300

    remove shadow

--- src/components/Header.js
+++ src/components/Header.js
@@ -1,12 +1,7 @@
 import * as React from 'react';
 import { useTheme } from '@mui/material/styles';
-import { Link, Stack, useMediaQuery } from '@mui/material';
+import { AppBar, Box, Container, IconButton, Link, Stack, Toolbar, Typography, useMediaQuery } from '@mui/material';
 import { pink, blue } from '@mui/material/colors';
-import AppBar from '@mui/material/AppBar';
-import Box from '@mui/material/Box';
-import Toolbar from '@mui/material/Toolbar';
-import Typography from '@mui/material/Typography';
-import IconButton from '@mui/material/IconButton';
 import MenuIcon from '@mui/icons-material/Menu';
 import TwitterIcon from '@mui/icons-material/Twitter';
 import InstagramIcon from '@mui/icons-material/Instagram';
@@ -17,34 +12,36 @@ const Header = ({handleDrawerOpen, drawerOpen}) => {
     const matchUpSm = useMediaQuery(theme.breakpoints.up('sm'));
     return (
         <Box sx={{ flexGrow: 1 }}>
-            <AppBar position="fixed">
-                <Toolbar>
-                    <IconButton
-                        size="large"
-                        edge="start"
-                        color="inherit"
-                        aria-label="menu"
-                        sx={{ mr: 2 }}
-                        onClick={handleDrawerOpen}
-                    >
-                        <MenuIcon fontSize="large" />
-                    </IconButton>
-                    <Typography variant="h1" component="div" sx={{ flexGrow: 1, display: matchUpSm ? 'block' : 'none' }}>
-                        BULL OR BEAR
-                    </Typography>
-                    <Stack flexDirection="row" alignItems="center" gap={2}>
-                        <Link href="">
-                            <TwitterIcon 
-                                fontSize="large"
-                                sx={{ color: blue[500] }} />
-                        </Link>
-                        <Link href="">
-                            <InstagramIcon 
-                                fontSize="large"
-                                sx={{ color: pink[500] }} />
-                        </Link>
-                    </Stack>
-                </Toolbar>
+            <AppBar position="fixed" sx={{ boxShadow: 'unset' }}>
+                <Container>
+                    <Toolbar>
+                        <IconButton
+                            size="large"
+                            edge="start"
+                            color="inherit"
+                            aria-label="menu"
+                            sx={{ mr: 2 }}
+                            onClick={handleDrawerOpen}
+                        >
+                            <MenuIcon fontSize="large" />
+                        </IconButton>
+                        <Typography variant="h1" component="div" sx={{ flexGrow: 1, display: matchUpSm ? 'block' : 'none' }}>
+                            Project Logo
+                        </Typography>
+                        <Stack flexDirection="row" alignItems="center" gap={2}>
+                            <Link href="">
+                                <TwitterIcon 
+                                    fontSize="large"
+                                    sx={{ color: blue[500] }} />
+                            </Link>
+                            <Link href="">
+                                <InstagramIcon 
+                                    fontSize="large"
+                                    sx={{ color: pink[500] }} />
+                            </Link>
+                        </Stack>
+                    </Toolbar>
+                </Container>
             </AppBar>
         </Box>
     );
